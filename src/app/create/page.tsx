343ff4461c67175c2674613fce2495409765906b
Scott Key <mscottkey@gmail.com>
2025-09-06 02:46:40 +0000

    Runtime Error NotAllowedError: Failed to execute 'writeText' on 'Clipboa

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -3,6 +3,7 @@
 
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
